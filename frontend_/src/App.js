7fab7a316218da50af0b9654bf6b301d50a87a63
FlexonaFFt <flexonafft@bk.ru>
2024-07-15 18:43:11 +0300

    --load badfrontend | --init frontend

---
+++ frontend_/src/App.js
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import "./App.css";
+import "./templates/MarkdownEditor";
+
+function App() {
+  return (
+    <div>
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+export default App;
